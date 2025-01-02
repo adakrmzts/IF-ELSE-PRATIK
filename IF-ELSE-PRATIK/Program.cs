4869cb3ade7f8dd7ea9c765575ab1757d83bfe93
Alpay <Alpay@ALPAY>
2025-01-02 23:08:47 +0300

    Add project files.

---
+++ IF-ELSE-PRATIK/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System.ComponentModel.Design;
+
+Console.Write("Lütfen bir sayı giriniz : ");
+
+// Girilen sayıyı değerini değişkene atadım. 
+
+int sayi = Convert.ToInt32(Console.ReadLine()); 
+
+if (sayi == 10) 
+{ 
+    Console.WriteLine("Sayı 10'dur.");
+}
+else if (sayi > 10)
+{   Console.WriteLine("Sayı 10'dan büyüktür.");
+}
+else
+{
+    Console.WriteLine("Sayı 10'dan küçüktür.");
+}
+
+// Üstte bu yapıyı kullanma sebebim if döngüsü true çevirirse sayının 10 ile ilgili başka bir ilişkisini kontrol etmek istemiyorum.
+
+
+// Alt kısımda yeniden if ile başlaa sebebim ise bu döngüye girmeyi zorlamam lazım ki çift mi tek mi olduğunu anlayım. 
+
+
+if (sayi % 2 == 0)
+{
+    Console.WriteLine("Sayı çifttir.");
+}
+else
+{
+    Console.WriteLine("Girilen sayı tektir.");
+}
